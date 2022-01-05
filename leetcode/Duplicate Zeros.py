a72695d3d50b1fbaf5e9df8cb696ad0c90b13193
thddlmy <thddlmy@gmail.com>
2022-01-05 21:11:19 +0900

    algo: [easy] array

---
+++ leetcode/Duplicate Zeros.py
@@ -0,0 +1,12 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        answer_temp = [] 
+        for num in arr: 
+            if num == 0: 
+                answer_temp.append(num) 
+            answer_temp.append(num) 
+            
+        answer = answer_temp[:len(arr)] 
+        
+        for i in range(len(arr)): 
+            arr[i] = answer[i]
