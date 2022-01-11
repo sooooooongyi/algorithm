8c98664598a8c2ce44030ea12956df41da841483
thddlmy <thddlmy@gmail.com>
2022-01-11 23:25:02 +0900

    study: 구현lv1

---
+++ 이것이취업을위한코딩테스트다/모험가길드.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+
+answer = 0
+count = 0
+
+for i in data:
+    count += 1
+    if count >= i:
+        answer += 1
+        count = 0
+
+print(answer)
