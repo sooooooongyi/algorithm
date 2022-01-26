a4e244ee58cd7068041100b6b3affd9d7307bbed
thddlmy <thddlmy@gmail.com>
2022-01-26 22:13:36 +0900

    study: 그리디 실버5

---
+++ baekjoon-online-judge/30.py
@@ -0,0 +1,13 @@
+N = input()
+N = sorted(N, reverse=True)
+sum = 0
+
+if '0' not in N:
+  print(-1)
+else:
+  for i in N:
+    sum += int(i)
+  if sum % 3 == 0:
+    print(''.join(N))
+  else:
+    print(-1)
