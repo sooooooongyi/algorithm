95b15ee4bd406d44a441eeafa070c07f45352cd6
sarah <sarah@mobidoo.co.kr>
2022-08-02 18:35:07 +0900

    study: 골드3 최단경로

--- baekjoon-online-judge/1238.py
+++ baekjoon-online-judge/1238.py
@@ -1,6 +1,4 @@
 # 다익스트라 알고리즘 - 파티
-# Solution
-# (1) 시간 초과 : 
 
 import sys, heapq
 input = sys.stdin.readline
@@ -8,74 +6,35 @@ INF = int(1e9)
 
 n, m, x = map(int, input().split())
 graph = [[]for i in range(n+1)]
-ans = [0] * (n+1)
-heap = []
+visited = [False] * (n+1)
 
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b,c))
 
-def dijkatra(start):
+def dijkstra(start):
+    q = []
     distance = [INF] * (n+1)
-    heapq.heappush(heap, [0, start]) # 비용, 노드
+    heapq.heappush(q, (0, start)) # 비용, 노드
     distance[start] = 0
 
-    while heap:
-        c, n = heapq.heappop(heap)
-        for nc, nn in graph[n]:
-            nc += c
-            if nw < distance[nn]:
-                distance[nn] = nw
-                heapq.heappush(heap, [nw, nn])
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
     return distance
 
+result = [[]]
+answer = []
 for i in range(1, n+1):
-    distance = dijkatra(i)
-    ans[i] += distance[x]
-    distance = dijkatra(x)
-    ans[i] += distance[i]
-
-print(max(ans))
-
-
-'''
-def get_smallest_node():
-    min_value = INF
-    index = 0
-    for i in range(1, n+1):
-        if distance[i] < min_value and not visited[i]:
-            min_value = distance[i]
-            index = i
-    return index
-
-def dijkstra(start):
-    distance[start] = 0
-    visited[start] = True
-
-    for i in graph[start]:
-        distance[i[0]] = i[1]
-
-    for i in range(n-1):
-        now = get_smallest_node()
-        visited[now] = True
-        for j in graph[now]:
-            cost = distance[now]+j[1]
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
+    result.append(dijkstra(i))
 
 for i in range(1, n+1):
-    dijkstra(i)
-    go_arr.append(distance[x])
-    distance = [INF for _ in range(n+1)]
-    visited = [False for _ in range(n+1)]
+    answer.append(result[i][x] + result[x][i])
 
-dijkstra(x)
-
-max_value = -1
-for i in range(1, n+1):
-    if go_arr[i]+distance[i] > max_value:
-        max_value = go_arr[i]+distance[i]
-
-print(max_value)
-'''
-
+print(max(answer))
