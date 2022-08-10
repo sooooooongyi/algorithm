ec0e8036ff3d719f6c7a2d15ba834d5b681a9101
sarah <sarah@mobidoo.co.kr>
2022-08-10 20:50:43 +0900

    study: 골드5 구현

--- baekjoon-online-judge/14503.py
+++ baekjoon-online-judge/14503.py
@@ -1,36 +1,36 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-
-answer = 1
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
 
 n, m = map(int, input().split())
 x, y, d = map(int, input().split())
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+answer = 1
 graph[x][y] = 2
 
 while True:
+    # 4방향 중 하나라도 벽이 아니라면 False
     check = False
     for i in range(4):
-        d = (d + 3)%4
+        d = (d+3) % 4
         nx = x + dx[d]
         ny = y + dy[d]
 
         if graph[nx][ny] == 0:
-            graph[nx][ny] = 2
             answer += 1
-            x, y = nx, ny
+            graph[nx][ny] = 2
+            x = nx
+            y = ny
             check = True
+            # 벽이 아닌 방향을 우선적으로 만나면 그 방향으로 가야함!
             break
+
     if not check:
-        if graph[x - dx[d]][y - dy[d]] == 1:
+        if graph[x-dx[d]][y-dy[d]] == 1:
             break
         else:
             x = x - dx[d]
