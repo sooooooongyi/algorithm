85e4c59538e613536c2ab8c950f5ce85da3f39a1
sarah <sarah@mobidoo.co.kr>
2022-08-12 00:02:31 +0900

    study: 골드4 구현 (로직 수정)

--- baekjoon-online-judge/14499.py
+++ baekjoon-online-judge/14499.py
@@ -1,45 +1,41 @@
-n, m, x, y, k = map(int, input().split())
+from __future__ import barry_as_FLUFL
+import sys
+input = sys.stdin.readline
 
-board = []
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
+
+n, m, x, y, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 dice = [0, 0, 0, 0, 0, 0]
 
 
-def turn(dir):
+def rolling(direction):
     a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
-    if dir == 1:  # 동
+    if direction == 1:
         dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
-
-    elif dir == 2:  # 서
+    if direction == 2:
         dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
-
-    elif dir == 3:  # 북
+    if direction == 3:
         dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
-
-    else:
+    if direction == 4:
         dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
 
 
-for i in range(n):
-    board.append(list(map(int, input().split())))
-
-comm = list(map(int, input().split()))
+commands = list(map(int, input().split()))
 
 nx, ny = x, y
-for i in comm:
-    nx += dx[i-1]
-    ny += dy[i-1]
-
-    if nx < 0 or nx >= n or ny < 0 or ny >= m:
-        nx -= dx[i-1]
-        ny -= dy[i-1]
-        continue
-    turn(i)
-    if board[nx][ny] == 0:
-        board[nx][ny] = dice[-1]
+for command in commands:
+    nx = x + dx[command-1]
+    ny = y + dy[command-1]
+    if 0 <= nx < n and 0 <= ny < m:
+        rolling(command)
+        if graph[nx][ny] == 0:
+            graph[nx][ny] = dice[-1]
+        else:
+            dice[-1] = graph[nx][ny]
+            graph[nx][ny] = 0
+        x, y = nx, ny
     else:
-        dice[-1] = board[nx][ny]
-        board[nx][ny] = 0
-
+        continue
     print(dice[0])
