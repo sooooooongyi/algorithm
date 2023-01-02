a75033c6b5b81173af94970827758db47512d23f
sarah <sarah@mobidoo.co.kr>
2023-01-02 23:13:31 +0900

    study: 구현 난이도1

---
+++ baekjoon-online-judge/2회독/1이될때까지.py
@@ -0,0 +1,19 @@
+# [구현] 난이도1
+
+N, K = map(int, input().split())
+answer = 0
+
+while True:
+  target = (N // K) * N
+  answer += (N - target)
+  N = target
+
+  if N < K:
+    break
+
+  N //= K
+  answer += 1
+
+answer += N - 1
+print(answer)
+  
