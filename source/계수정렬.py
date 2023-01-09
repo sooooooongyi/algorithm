c70444e8621ed95618213b0a195e595fcc46fe5a
sarah <sarah@mobidoo.co.kr>
2023-01-09 23:39:06 +0900

    study: 정렬 알고리즘 소스코드

---
+++ source/계수정렬.py
@@ -0,0 +1,11 @@
+# [정렬] 계수정렬
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+count = [0] * max(len(array) +1)
+
+for i in range(len(array)):
+  count[array[i]] += 1
+
+  for i in range(len(count)):
+    for j in range(count[i]):
+      print(i, end=' ')
