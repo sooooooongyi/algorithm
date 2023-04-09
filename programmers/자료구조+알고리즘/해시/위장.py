3c88b5ddf0def75d76279533deb7838203b285ec
sarah <sarah@mobidoo.co.kr>
2023-04-09 23:42:26 +0900

    algorithm: 위장

---
+++ programmers/자료구조+알고리즘/해시/위장.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+def solution(clothes):
+    answer = 1
+    dic = defaultdict(list)
+    for i in clothes:
+        dic[i[1]].append(i[0])
+    for k in dic:
+        answer*=len(dic[k]) + 1
+    answer -= 1
+    return answer
