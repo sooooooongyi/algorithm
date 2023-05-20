1cd93df257badb840ac3ccc94b90e18999bfdddd
sarah <sarah@mobidoo.co.kr>
2023-05-21 01:16:04 +0900

    chore: 주석 제거

--- programmers/자료구조+알고리즘/스택+큐/다리를지나는트럭.py
+++ programmers/자료구조+알고리즘/스택+큐/다리를지나는트럭.py
@@ -1,39 +1,3 @@
-# from collections import deque
-
-# def solution(bridge_length, weight, truck_weights):
-#     bridge = deque([0] * bridge_length)
-#     trucks = deque(truck_weights)
-#     seconds = 1
-#     truck_count = 1
-#     bridge_sum = sum(bridge)
-    
-#     bridge[-1] = truck_weights[0]
-#     trucks.popleft()
-    
-#     while truck_count and not all(i ==0 for i in bridge):
-#         bridge_sum -= bridge[0]
-#         bridge_sum += bridge[-1]
-#         # 시간이 지나, 위치 이동
-#         if bridge.popleft() != 0:
-#             truck_count -= 1
-#         seconds += 1
-#         # 다리 위에 못 올라가는 경우
-#         if not trucks: 
-#             bridge.append(0)
-#             continue
-#         if truck_count > bridge_length or (bridge_sum + trucks[0]) > weight: 
-#             bridge.append(0)
-#             continue
-#         # 다리 위에 올라가는 경우
-#         bridge.append(trucks.popleft())
-#         truck_count += 1
-
-#     return seconds
-
-# # print(solution(2, 10, [7, 4, 5, 6]))
-# # print(solution(100, 100, [10]))
-# print(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]))
-
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
