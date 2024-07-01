c15d40cb4d10c2352410b670f2e54d63d70daedd
sarah <sarah@mobidoo.co.kr>
2024-07-02 00:14:45 +0900

    alorithm: 2024-07-02 괄호변환 재풀이

--- 이것이취업을위한코딩테스트다/기출문제/dfs+bfs/괄호변환.py
+++ 이것이취업을위한코딩테스트다/기출문제/dfs+bfs/괄호변환.py
@@ -1,41 +1,45 @@
-def check(s):
-    stack = []
-
-    for i in s:
-        if i == '(':
-            stack.append(i)
-        else:
-            if len(stack) == 0:
-                return False
-            stack.pop()
-    return True
-
-def divide(s):
-    left, right = 0,0
-    for i in range(len(s)):
-        if s[i] == '(':
-            left += 1
-        else:
-            right += 1
-        if left == right:
-            return s[:i+1], s[i+1:]
-
-def solution(p):
-    if not p:
-        return ''
-
-    u, v = divide(p)
-
-    if check(u):
-        return u + solution(v)
+def get_balanced_index(p):
+  count = 0
+  for i in range(len(p)):
+    if p[i] == '(':
+      count += 1
     else:
-        answer = '('
-        answer += solution(v)
-        answer += ')'
+      count -= 1
+    if count == 0:
+      return i
+    
+def get_proper_string(p):
+  count = 0
+  for i in p:
+    if i == '(':
+      count += 1
+    else:
+      if count == 0:
+        return False
+      count -= 1
+    return True
+  
+def solution(p):
+  answer = ''
+  if p == '':
+    return ''
+  
+  index = get_balanced_index(p)
+  u = p[:index+1]
+  v = p[index+1:]
 
-        for s in u[1:len(u)-1]:
-            if s == '(':
-                answer += ')'
-            else:
-                answer += '('
-        return answer
+  if get_proper_string(u):
+    answer = u + solution(v)
+  else:
+    answer = '('
+    answer += solution(v)
+    answer += ')'
+    u = list(u[1:-1])
+    for i in range(len(u)):
+      if u[i] == '(':
+        u[i] = ')'
+      else:
+        u[i] = '('
+    answer += ''.join(u)
+
+  return answer
