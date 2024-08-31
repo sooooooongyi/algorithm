769e4078a65aa5570aa3e2bcf32cd56d7da14ff1
sooooooongyi <sooooooongyi@gmail.com>
2024-08-31 18:03:26 +0900

    algorithm: 2024-08-31

--- baekjoon-online-judge/1회독/[10431]줄세우기.py
+++ baekjoon-online-judge/1회독/[10431]줄세우기.py
@@ -1,16 +1,16 @@
-# 🌜, ✨ 리스트 순회하며, 나보다 큰 숫자들 계속 더하기.
+# 🌜, ✨ 입력된 리스트를 순회하며, 나보다 큰 학생들(한 칸씩 밀릴 학생) 수 구하기
 
 p = int(input())
 answer = 0
 
-for _ in range(p):
+for i in range(p):
     answer = 0
-    arrList = list(map(int, input().split()))
+    arrList = list(map(int, input().split()))[1:]
     arrTest = []
     
-    for i in arrList:
-        arrTest.append(i)
+    for j in arrList:
+        arrTest.append(j)
         arrTest.sort()
-        answer += len(arrTest) - (arrTest.index(i) + 1)
+        answer += len(arrTest) - (arrTest.index(j) + 1)
         
-    print(p+1, answer)
+    print(i+1, answer)
